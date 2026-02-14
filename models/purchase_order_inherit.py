fb4281f40fbb6353f571ea30172d5f24b2e48160
souhaibouadi <147671244+souhaibouadi@users.noreply.github.com>
2026-02-15 00:10:05 +0100

    Add missing fields to purchase.order inheritance
    Added fields referenced by purchase_order_inherit_views.xml:
    - procurement_award_id (Many2one)
    - procurement_service_order_id (Many2one)
    - procurement_contract_number (Char)
    - procurement_contract_date (Date)
    - is_public_procurement (Boolean)

--- models/purchase_order_inherit.py
+++ models/purchase_order_inherit.py
@@ -5,5 +5,17 @@ from odoo import models, fields, api
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
+    # Procurement Reference Fields
     procurement_procedure_id = fields.Many2one(
         'procurement.procedure', string="Linked Procurement Procedure")
+    procurement_award_id = fields.Many2one(
+        'procurement.award', string="Linked Procurement Award")
+    procurement_service_order_id = fields.Many2one(
+        'procurement.service.order', string="Linked Service Order")
+    
+    # Contract Information
+    procurement_contract_number = fields.Char(string="Contract Number")
+    procurement_contract_date = fields.Date(string="Contract Date")
+    is_public_procurement = fields.Boolean(
+        string="Is Public Procurement", default=False,
+        help="Check if this purchase order is part of a public procurement process")
