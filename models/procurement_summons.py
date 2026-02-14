97d4237edde7c2237c4dcd5c6cbfc9a3bac8ce04
souhaibouadi <147671244+souhaibouadi@users.noreply.github.com>
2026-02-14 10:57:05 +0100

    Add member_id field to procurement.summons model

--- models/procurement_summons.py
+++ models/procurement_summons.py
@@ -10,6 +10,7 @@ class ProcurementSummons(models.Model):
     procedure_id = fields.Many2one('procurement.procedure', string="Procedure")
     session_id = fields.Many2one('procurement.committee.session', string="Session")
     recipient_id = fields.Many2one('res.partner', string="Recipient", required=True)
+        member_id = fields.Many2one('procurement.committee.member', string="Committee Member")
     role = fields.Char(string="Role in Committee", help="Vice-President, Secretary, Member, etc.")
     organization = fields.Char(string="Affiliated Organization", help="e.g. DECC, DAG, DOF, CNR, FNPOS, CA, etc.")
     send_date = fields.Date(string="Sending Date")
